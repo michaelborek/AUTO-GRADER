cd6d2983bd90e46ebed7e0b1eb6f287528759d44
michaelborek <borekmi1@msu.edu>
2023-08-14 19:00:44 +0200

    Update to documentation and pytests
    Splitted pytests to make it easier for debugging, also worked on project2_test.py which occured to have logic errors. At the end updated also documentation for these pytests.

--- src/project2_test.py
+++ src/project2_test.py
@@ -3,137 +3,87 @@ from dataclasses import dataclass
 
 @dataclass
 class MockInput:
-    """
-    A utility data class for mocking user input during testing.
+    """A utility data class for mocking user input during testing.
 
     Attributes
     ----------
-    test_rock_input : str
+    test_some_input : str
         Test case where player picks rock.
-    test_paper_input : str
-        Test case where player picks paper.
-    test_scissors_input : str
-        Test case where player picks scissors.
-    test_lizard_input : str
-        Test case where player picks lizard.
-    test_spock_input : str
-        Test case where player picks spock.
     """
 
-    test_rock_input: str = '1'
-    test_paper_input: str = '2'
-    test_scissors_input: str = '3'
-    test_lizard_input: str = '4'
-    test_spock_input: str = '5'
+    test_some_input: str = '1'  # Test case where player picks rock
 
-@dataclass
-class MockRandom:
+def test_randomness_of_computer_guess(mocker):
+    """Test the randomness of computer's guess by checking a set of 1000 guesses."""
+    
+    Mock_Input = MockInput()
+
+    guesses = set()
+    mocker.patch('project2.input', return_value=Mock_Input.test_some_input)
+
+    for _ in range(1000):
+        game = project2.GamesRules()
+        guesses.add(game.computer_guess)
+
+    assert len(guesses) >= 3
+
+def test_game_logic(mocker, capfd):
+    """Test the game logic based on RPSLS rules.
+    
+    The function patches the input and random.randint functions from project2 
+    and checks the game's logic for certain matchups.
     """
-    A utility data class for mocking random numbers during testing.
+    
+    matchups = [
+        (1, 3), (1, 4),  # Rock wins
+        (2, 1), (2, 5),  # Paper wins
+        (3, 2), (3, 4),  # Scissors wins
+        (4, 2), (4, 5),  # Lizard wins
+        (5, 1), (5, 3)   # Spock wins
+    ]
 
-    Attributes
-    ----------
-    test_rock_random : int
-        Test case where computer picks rock.
-    test_paper_random : int
-        Test case where computer picks paper.
-    test_scissors_random : int
-        Test case where computer picks scissors.
-    test_lizard_random : int
-        Test case where computer picks lizard.
-    test_spock_random : int
-        Test case where computer picks spock.
-    """
+    for player, computer in matchups:
+        mocker.patch('project2.input', return_value=str(player))
+        mocker.patch('project2.random.randint', return_value=computer)
+        project2.main()
+        
+        out, _ = capfd.readouterr()
+        assert 'You win' in out
 
-    test_rock_random: int = 1 
-    test_paper_random: int = 2
-    test_scissors_random: int = 3
-    test_lizard_random: int = 4
-    test_spock_random: int = 5
+def test_game_output_messages(mocker, capfd):
+    """Test the game's output messages for various game outcomes.
 
-def test_RPSLS_game(mocker, capfd):
-    """
-    Test to verify the RPSLS game workflow and correctness from `project2.py`.
-
-    This test ensures that the game randomly picks number, which refers to one of the five options, 
-    prints correct outputs like 'I guessed...' as required, finds the winner correctly, 
-    prints correct statements like 'scissors cuts paper' etc.
+    The function patches the input and random.randint functions from project2 
+    and checks the game's output messages for each matchup in test_data.
     
     Parameters
     ----------
-    mocker : fixture
-        A pytest fixture for mocking. Used to mock `input` and `random.randint`.
-    capfd : fixture
-        A pytest fixture to capture stdout and stderr.
-
-    Returns
-    -------
-    None
+    mocker : object
+        The mock object used to replace real functions or methods with mock calls.
+    capfd : object
+        Capture, as text, output to file descriptors 1 and 2.
+        
+    Notes
+    -----
+    The test checks if both the computer's move and the game outcome message 
+    are correctly printed to the console.
     """
-    Inputs_Mock = MockInput()
-    Random_Mock = MockRandom()
+    
+    test_data = [
+        (1, 3, 'I guessed scissors', 'You win because rock crushes scissors'),
+        (1, 4, 'I guessed lizard', 'You win because rock crushes lizard'),
+        (1, 1, 'I guessed rock','Tie! We both guessed rock'),
+        (3, 2, 'I guessed paper', 'You win because scissors cuts paper'),
+        (3, 4, 'I guessed lizard', 'You win because scissors decapitates lizard')
+        # Add other combinations as needed
+    ]
 
-    # Logic for checking if the game has random int for every game
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_paper_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_rock_input)
-    project2.main()
-    out,err = capfd.readouterr()
-    print(out)
-    assert "I guessed paper" in out
-
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_spock_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_spock_input)
-    project2.main()
-    out,err = capfd.readouterr()
-    print(out)
-    assert "I guessed spock" in out
-
-    # Test 2: Checks if game has a correct logic for wins/loses/ties
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_rock_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_rock_input)
-    project2.main()
-    out, err = capfd.readouterr()
-    assert "Tie!" in out
-
-    # Logic Win Paper over Rock
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_rock_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_paper_input)
-    project2.main()
-    out, err = capfd.readouterr()
-    assert "You win" in out
-
-    # Logic Lose Rock over Scissors
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_rock_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_scissors_input)
-    project2.main()
-    out, err = capfd.readouterr()
-    assert "You lose" in out
-
-    # Logic Lose Rock over Lizard
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_rock_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_lizard_input)
-    project2.main()
-    out, err = capfd.readouterr()
-    assert "You lose" in out
-
-    # Logic Win Spocks over Rock
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_rock_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_spock_input)
-    project2.main()
-    out, err = capfd.readouterr()
-    assert "You win" in out
-
-    # Test 3: Correct console outputs
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_paper_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_scissors_input)
-    project2.main()
-    out, err = capfd.readouterr()
-    assert 'I guessed paper' in out
-    assert 'You win because scissors cuts paper' in out
-
-    mocker.patch('project2.random.randint', return_value=Random_Mock.test_scissors_random)
-    mocker.patch('project2.input', side_effect=Inputs_Mock.test_lizard_input)
-    project2.main()
-    out, err = capfd.readouterr()
-    assert 'I guessed scissors' in out
-    assert 'You lose because scissors decapitates lizard' in out
+    for player, computer, computer_message, outcome_message in test_data:
+        mocker.patch('project2.input', return_value=str(player))
+        mocker.patch('project2.random.randint', return_value=computer)
+        
+        project2.main()
+        
+        out, _ = capfd.readouterr()
+        assert computer_message in out
+        assert outcome_message in out
